3d58d8bd12d47d37fae81ce8bec8f16e238e8e0e
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-01-30 10:22:43 +0100

    Libreria creada

---
+++ CalculoIVA_DLL/CalculoIVA.vb
@@ -0,0 +1,37 @@
+﻿Public Class CalculoIVA
+
+    Private total As Double
+    Private Enum tipoIVAEnum
+        general = 0.21
+        reducido = 0.1
+        productos_basicos = 0.05
+    End Enum
+
+    Private tipoIVA As tipoIVAEnum
+
+    Private precioBase As Double
+    Private IVA As Double
+
+    Public Sub New()
+        tipoIVA = tipoIVAEnum.general
+    End Sub
+
+
+    Public Function TotalSinIVA(totalPrecio As Double, tipo As String) As Double
+        If (tipoIVAEnum.general = tipo) Then
+            tipoIVA = tipoIVAEnum.general
+        ElseIf (tipoIVAEnum.reducido = tipo) Then
+            tipoIVA = tipoIVAEnum.reducido
+        ElseIf (tipoIVAEnum.productos_basicos = tipo) Then
+            tipoIVA = tipoIVAEnum.productos_basicos
+        End If
+        precioBase = totalPrecio / (1 + tipoIVA)
+        Return precioBase
+    End Function
+
+    Public Function CalcularIVA(totalPrecio As Double, tipo As String) As Double
+        IVA = totalPrecio - TotalSinIVA(totalPrecio, tipo)
+        Return IVA
+    End Function
+
+End Class
