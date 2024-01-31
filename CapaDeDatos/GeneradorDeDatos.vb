f397c583f7b727d198f0657af3b9f423385168ba
Daria <dpaslavska001@ikasle.ehu.eus>
2024-01-31 13:10:18 +0100

    Capa de Datos hecha 3.1

---
+++ CapaDeDatos/GeneradorDeDatos.vb
@@ -0,0 +1,25 @@
+﻿Public Class GeneradorDeDatos
+
+    Private Shared ImportesFacturasYTiposIVA As Double(,) =
+        {{2500, 10}, {3150.55, 21}, {25000, 21}, {315.35, 4}}
+
+    Public Shared Function ObtenerTotalFactura(numeroFactura As Integer) As Double
+        If numeroFactura > 0 AndAlso numeroFactura <= ImportesFacturasYTiposIVA.GetLength(0) Then
+
+            Return ImportesFacturasYTiposIVA(numeroFactura - 1, 0)
+        Else
+            Throw New ArgumentException("Número de factura no válido")
+        End If
+    End Function
+
+
+    Public Shared Function ObtenerTipoIVA(numeroFactura As Integer) As Integer
+        If numeroFactura > 0 AndAlso numeroFactura <= ImportesFacturasYTiposIVA.GetLength(0) Then
+
+            Return CInt(ImportesFacturasYTiposIVA(numeroFactura - 1, 1))
+        Else
+            Throw New ArgumentException("Número de factura no válido")
+        End If
+    End Function
+
+End Class
