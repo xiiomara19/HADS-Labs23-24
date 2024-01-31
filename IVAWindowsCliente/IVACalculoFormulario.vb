70a3fd70d34faf9cd8308f6391cd25906f9ce426
Daria <dpaslavska001@ikasle.ehu.eus>
2024-01-31 18:50:40 +0100

    Cambios en IVAWindowsCliente

--- IVAWindowsCliente/IVACalculoFormulario.vb
+++ IVAWindowsCliente/IVACalculoFormulario.vb
@@ -1,13 +1,20 @@
 ﻿Public Class IVACalculoFormulario
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim c As New CalculoIVA_DLL.CalculoIVA(1000, 21)
-        Dim prueba As String
 
-        prueba = ComboBox1.SelectedItem
-        ' Validate that TextBox1.Text is a valid number
-        'TextBox2.Text = c.TotalSinIVA(CDbl(TextBox1.Text), ComboBox1.SelectedItem)
+
+        Dim pTotalIVA As Byte
+        Dim selectedValue As String = ComboBox1.SelectedItem.ToString()
+
+        If (selectedValue = "general") Then
+            pTotalIVA = 21
+        ElseIf (selectedValue = "reducido") Then
+            pTotalIVA = 10
+        ElseIf (selectedValue = "productos basicos") Then
+            pTotalIVA = 5
+        End If
+        Dim c As New CalculoIVA_DLL.CalculoIVA(CDbl(TextBox1.Text), pTotalIVA)
+
         TextBox2.Text = c.TotalSinIVA()
-        'TextBox3.Text = c.CalcularIVA(CDbl(TextBox1.Text), ComboBox1.SelectedItem)
         TextBox3.Text = c.CalcularIVA()
     End Sub
 
