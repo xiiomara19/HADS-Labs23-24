244574f2aea53d2d8855b1d2004bec3c5e682094
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-02-01 20:47:19 +0100

    Aplicación consola "Principal" creada, ejercicio 3.3

---
+++ IVAConsolaPorCapaDeDatos/Principal.vb
@@ -0,0 +1,15 @@
+﻿Module Principal
+
+    Sub Main()
+        Console.WriteLine("Inserte un número de factura: ")
+        Dim CodigoFactura As Integer = Console.ReadLine
+        'No entiendo porque no funciona
+        'Dim c As New CalculoIvaPorCapaDeDatos_DLL.CalculoIVAMedianteCreadorDeDatos(CodigoFactura)
+
+        Console.WriteLine("Total de la factura sin IVA: {0}")
+        Console.WriteLine("IVA: {0}")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
