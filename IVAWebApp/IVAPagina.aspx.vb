d1e75e71b40b195afed59885d38168e8b1b07b1b
heyyder <efernandez200@ikasle.ehu.eus>
2024-02-02 19:06:15 +0100

    Tratar números negativos

--- IVAWebApp/IVAPagina.aspx.vb
+++ IVAWebApp/IVAPagina.aspx.vb
@@ -8,9 +8,16 @@
     Protected Sub CalcularBtn_Click(sender As Object, e As EventArgs) Handles CalcularBtn.Click
         ErrorLbl.Text = ""
         Try
-            Dim c As New CalculoIVA_DLL.CalculoIVA(TotalInput.Value, tipoIVAList.SelectedValue)
-            PrecioBaseTxtBox.Text = c.TotalSinIVA()
-            IVAAplicadoTxtBox.Text = c.IVA()
+            If (TotalInput.Value > 0) Then
+                Dim c As New CalculoIVA_DLL.CalculoIVA(TotalInput.Value, tipoIVAList.SelectedValue)
+                PrecioBaseTxtBox.Text = c.TotalSinIVA()
+                IVAAplicadoTxtBox.Text = c.IVA()
+            Else
+                PrecioBaseTxtBox.Text = ""
+                IVAAplicadoTxtBox.Text = ""
+                ErrorLbl.Text = "Se debe introducir un valor positivo."
+            End If
+
         Catch ex As Exception
             PrecioBaseTxtBox.Text = ""
             IVAAplicadoTxtBox.Text = ""
--- IVAWindowsCliente/IVACalculoFormulario.Designer.vb
+++ IVAWindowsCliente/IVACalculoFormulario.Designer.vb
@@ -34,71 +34,80 @@ Partial Class IVACalculoFormulario
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
+        Me.errorLbl = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(106, 66)
+        Me.Label1.Location = New System.Drawing.Point(80, 54)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(162, 17)
+        Me.Label1.Size = New System.Drawing.Size(120, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Cantidad a pagar (total):"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(106, 124)
+        Me.Label2.Location = New System.Drawing.Point(80, 101)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(65, 17)
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Tipo IVA:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(106, 260)
+        Me.Label3.Location = New System.Drawing.Point(80, 211)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(91, 17)
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Total sin IVA:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(106, 316)
+        Me.Label4.Location = New System.Drawing.Point(80, 257)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(33, 17)
+        Me.Label4.Size = New System.Drawing.Size(27, 13)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "IVA:"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(313, 66)
+        Me.TextBox1.Location = New System.Drawing.Point(235, 54)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(146, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(110, 20)
         Me.TextBox1.TabIndex = 4
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(313, 255)
+        Me.TextBox2.Location = New System.Drawing.Point(235, 207)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(146, 22)
+        Me.TextBox2.Size = New System.Drawing.Size(110, 20)
         Me.TextBox2.TabIndex = 5
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(313, 313)
+        Me.TextBox3.Location = New System.Drawing.Point(235, 254)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(146, 22)
+        Me.TextBox3.Size = New System.Drawing.Size(110, 20)
         Me.TextBox3.TabIndex = 6
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(480, 70)
+        Me.Label5.Location = New System.Drawing.Point(360, 57)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(16, 17)
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
         Me.Label5.TabIndex = 7
         Me.Label5.Text = "€"
         '
@@ -106,16 +115,18 @@ Partial Class IVACalculoFormulario
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"general", "reducido", "productos basicos"})
-        Me.ComboBox1.Location = New System.Drawing.Point(313, 124)
+        Me.ComboBox1.Location = New System.Drawing.Point(235, 101)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(146, 24)
+        Me.ComboBox1.Size = New System.Drawing.Size(110, 21)
         Me.ComboBox1.TabIndex = 8
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(220, 192)
+        Me.Button1.Location = New System.Drawing.Point(165, 156)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(109, 38)
+        Me.Button1.Size = New System.Drawing.Size(82, 31)
         Me.Button1.TabIndex = 9
         Me.Button1.Text = "Calcular"
         Me.Button1.UseVisualStyleBackColor = True
@@ -123,26 +134,37 @@ Partial Class IVACalculoFormulario
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(483, 260)
+        Me.Label6.Location = New System.Drawing.Point(362, 211)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(16, 17)
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
         Me.Label6.TabIndex = 10
         Me.Label6.Text = "€"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(486, 315)
+        Me.Label7.Location = New System.Drawing.Point(364, 256)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(16, 17)
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
         Me.Label7.TabIndex = 11
         Me.Label7.Text = "€"
         '
+        'errorLbl
+        '
+        Me.errorLbl.AutoSize = True
+        Me.errorLbl.Location = New System.Drawing.Point(270, 165)
+        Me.errorLbl.Name = "errorLbl"
+        Me.errorLbl.Size = New System.Drawing.Size(0, 13)
+        Me.errorLbl.TabIndex = 12
+        '
         'IVACalculoFormulario
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.errorLbl)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Button1)
@@ -155,6 +177,7 @@ Partial Class IVACalculoFormulario
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "IVACalculoFormulario"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -174,4 +197,5 @@ Partial Class IVACalculoFormulario
     Friend WithEvents Button1 As Button
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
+    Friend WithEvents errorLbl As Label
 End Class
