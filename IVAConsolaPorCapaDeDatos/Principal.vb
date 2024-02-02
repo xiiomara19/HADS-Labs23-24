44415785acb3ea3010c7f212dc2ee4b9090402a4
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-02-02 18:36:28 +0100

    3.3 funciona correctamente + se ha tratado el primer ejercicio opcional (ej2)

--- IVAConsolaPorCapaDeDatos/Principal.vb
+++ IVAConsolaPorCapaDeDatos/Principal.vb
@@ -1,13 +1,26 @@
 ﻿Module Principal
 
     Sub Main()
-        Console.WriteLine("Inserte un número de factura: ")
-        Dim CodigoFactura As Integer = Console.ReadLine
-        'No entiendo porque no funciona
-        Dim c As New CalculoIvaPorCapaDeDatos_DLL.CalculoIVAMedianteCreadorDeDatos(CodigoFactura)
+        Try
+            Dim esNum As Boolean
+            Console.WriteLine("Inserte un número de factura: ")
+            Dim CodigoFactura As Integer
+            While Not esNum
+                CodigoFactura = Console.ReadLine
+                If (CodigoFactura < 1 Or CodigoFactura > 4) Then
+                    Console.WriteLine("El código de la factura debe ser un valor entre 1 y 4.")
+                Else
+                    esNum = True
+                End If
+            End While
 
-        Console.WriteLine("Total de la factura sin IVA: {0}")
-        Console.WriteLine("IVA: {0}")
+            Dim c As New CalculoIvaPorCapaDeDatos_DLL.CalculoIVAMedianteCreadorDeDatos(CodigoFactura)
+
+            Console.WriteLine("Total de la factura sin IVA: {0}", c.TotalSinIva())
+            Console.WriteLine("IVA: {0}", c.IVA())
+        Catch ex As Exception
+            Console.WriteLine("Debe insertar un valor numérico positivo entre 1 y 4.")
+        End Try
 
         Console.ReadLine()
     End Sub
