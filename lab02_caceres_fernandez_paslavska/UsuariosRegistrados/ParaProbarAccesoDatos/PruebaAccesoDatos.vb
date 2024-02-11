4a3495f11677a7bcd9f9e90770cbe2aed0269489
heyyder <efernandez200@ikasle.ehu.eus>
2024-02-11 01:27:26 +0100

    Lectura de los datos obtenidos hecho correctamente

--- lab02_caceres_fernandez_paslavska/UsuariosRegistrados/ParaProbarAccesoDatos/PruebaAccesoDatos.vb
+++ lab02_caceres_fernandez_paslavska/UsuariosRegistrados/ParaProbarAccesoDatos/PruebaAccesoDatos.vb
@@ -9,10 +9,13 @@ Module PruebaAccesoDatos
             Console.WriteLine("Conectando a la BD...")
             da.Conectar()
             Console.WriteLine("Conexión a la BD realizada con éxito.")
-            da.IncluirUsuario("d@gmail.com", "Daria", "Paslavska", "Que tal?", "Bien", 22, 22, True, 33, 32, "Alumno", 1111)
-            Dim usuario As SqlClient.SqlDataReader = da.ObtenerUsuarios("d@gmail.com")
-            Console.WriteLine(usuario.GetString(0))
+            'da.IncluirUsuario("d@gmail.com", "Daria", "Paslavska", "Que tal?", "Bien", 22, 22, True, 33, 32, "Alumno", 1111)
+            Dim usuario As SqlClient.SqlDataReader = da.ObtenerUsuarios("eider@gmail.com")
+            usuario.Read()
+            Console.WriteLine(usuario.GetString(1))
+            Console.WriteLine("Cerrando conexión con la BD...")
             da.CerrarConexion()
+            Console.WriteLine("Conexión con la BD cerrada.")
         Catch ex As Exception
             Console.WriteLine(ex.Message)
         End Try
