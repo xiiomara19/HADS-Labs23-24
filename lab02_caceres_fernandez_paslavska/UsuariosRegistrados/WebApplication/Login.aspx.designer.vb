c583a54d3b2a9de87cbfeae0bf09dbfb6cc5c630
Daria <dpaslavska001@ikasle.ehu.eus>
2024-02-13 17:45:39 +0100

    Recuperar contraseña hecho + control de errores

--- lab02_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Login.aspx.designer.vb
+++ lab02_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Login.aspx.designer.vb
@@ -95,13 +95,13 @@ Partial Public Class WebForm1
     Protected WithEvents HyperLink1 As Global.System.Web.UI.WebControls.HyperLink
 
     '''<summary>
-    '''HyperLink2 control.
+    '''recuperarLink control.
     '''</summary>
     '''<remarks>
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents HyperLink2 As Global.System.Web.UI.WebControls.HyperLink
+    Protected WithEvents recuperarLink As Global.System.Web.UI.WebControls.HyperLink
 
     '''<summary>
     '''Message control.
