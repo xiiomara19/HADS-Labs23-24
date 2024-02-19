9e9497616527ef356b39b5ceb26eec206b6274b5
heyyder <efernandez200@ikasle.ehu.eus>
2024-02-19 20:01:29 +0100

    vista trabajosGenericosAlumnos en proceso

--- lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/TrabajosGenericosAlumnado.aspx
+++ lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/TrabajosGenericosAlumnado.aspx
@@ -6,11 +6,48 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
-</head>
-<body>
+    <style type="text/css">
+        .auto-style2 {
+            height: 129px;
+        }
+        .auto-style3 {
+            height: 125px;
+        }
+    </style>
+    </head>
+<body style="height: 39px">
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <asp:Panel ID="Panel1" runat="server" BackColor="#CCCCCC" Height="55px" HorizontalAlign="Center" Width="504px">
+            ALUMNO<br /> GESTIÓN DE TRABAJOS GENÉRICOS</asp:Panel>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+   <table class="auto-style2">
+        <tr>
+            <td class="auto-style3">
+                <br />
+                <asp:DropDownList ID="asignaturasDDL" runat="server">
+                </asp:DropDownList>
+                <asp:Panel ID="Panel2" runat="server" Width="169px">
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="Button" />
+                </asp:Panel>
+            </td>
+            <td class="auto-style3">
+                <asp:Panel ID="Panel3" runat="server" Width="251px">
+                    <asp:CheckBoxList ID="caractTareasCBL" runat="server">
+                        <asp:ListItem Value="kodea">Código</asp:ListItem>
+                        <asp:ListItem Value="deskribapena">Descripción</asp:ListItem>
+                        <asp:ListItem Value="aurreikusitakoOrduak">Horas previstas</asp:ListItem>
+                        <asp:ListItem Value="lanMota">Tipo de trabajo</asp:ListItem>
+                    </asp:CheckBoxList>
+                </asp:Panel>
+            </td>
+        </tr>
+       <tr>
+            <asp:GridView ID="TareasGV" runat="server" AllowSorting="True">
+            </asp:GridView>
+       </tr>
+        </table> 
+
     </form>
 </body>
 </html>
--- lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/TrabajosGenericosAlumnado.aspx.designer.vb
+++ lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/TrabajosGenericosAlumnado.aspx.designer.vb
@@ -21,4 +21,76 @@ Partial Public Class WebForm8
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control Panel1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel1 As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control asignaturasDDL.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents asignaturasDDL As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control Panel2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel2 As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control Panel3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel3 As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control caractTareasCBL.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents caractTareasCBL As Global.System.Web.UI.WebControls.CheckBoxList
+
+    '''<summary>
+    '''Control TareasGV.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TareasGV As Global.System.Web.UI.WebControls.GridView
 End Class
