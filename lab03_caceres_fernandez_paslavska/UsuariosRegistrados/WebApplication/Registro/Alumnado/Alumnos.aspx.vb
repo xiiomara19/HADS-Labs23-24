b2e4c2fbb0ba4550acfdf864e63542ab6a41c26b
Daria <dpaslavska001@ikasle.ehu.eus>
2024-02-19 11:49:20 +0100

    Redirect correcto desde elumno o profesor a csmbiar contraseña

--- lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/Alumnos.aspx.vb
+++ lab03_caceres_fernandez_paslavska/UsuariosRegistrados/WebApplication/Registro/Alumnado/Alumnos.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Session("Modificar") = "Alumno"
     End Sub
 
 End Class
