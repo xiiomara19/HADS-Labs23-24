6003c6ad5118d3bf52fd1cdbf1895bd498098f75
heyyder <efernandez200@ikasle.ehu.eus>
2024-03-25 20:57:39 +0100

    carpeta de proyecto anterior borrada y register creado

--- lab5_caceres_fernandez_paslavska/lab5_caceres_fernandez_paslavska/urls.py
+++ lab5_caceres_fernandez_paslavska/lab5_caceres_fernandez_paslavska/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sitioDeFilms import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.register, name='register')
 ]
