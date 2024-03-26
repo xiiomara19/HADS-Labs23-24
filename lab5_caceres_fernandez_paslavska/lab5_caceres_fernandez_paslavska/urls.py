a2c2aff4d54dcbf19d02d932096c6637460078b3
heyyder <efernandez200@ikasle.ehu.eus>
2024-03-26 11:51:51 +0100

    boton submit creado

--- lab5_caceres_fernandez_paslavska/lab5_caceres_fernandez_paslavska/urls.py
+++ lab5_caceres_fernandez_paslavska/lab5_caceres_fernandez_paslavska/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from sitioDeFilms import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.register, name='register')
 ]
--- lab5_caceres_fernandez_paslavska/sitioDeFilms/views.py
+++ lab5_caceres_fernandez_paslavska/sitioDeFilms/views.py
@@ -5,5 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context = {'form': form}
     return render(request, 'register.html', context)
