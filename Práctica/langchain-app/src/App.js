96c015b16b8cc586260f7688c02624be061a7e8f
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-04-29 11:06:44 +0200

    teclado añadido

--- Práctica/langchain-app/src/App.js
+++ Práctica/langchain-app/src/App.js
@@ -17,6 +17,36 @@ function App() {
           <div class="Game-keyboard">
             <div class="Game-keyboard-row">
               <button class="Game-keyboard-button" type="button" id="a">Q</button>
+              <button class="Game-keyboard-button" type="button" id="a">W</button>
+              <button class="Game-keyboard-button" type="button" id="a">E</button>
+              <button class="Game-keyboard-button" type="button" id="a">R</button>
+              <button class="Game-keyboard-button" type="button" id="a">T</button>
+              <button class="Game-keyboard-button" type="button" id="a">Y</button>
+              <button class="Game-keyboard-button" type="button" id="a">U</button>
+              <button class="Game-keyboard-button" type="button" id="a">I</button>
+              <button class="Game-keyboard-button" type="button" id="a">O</button>
+              <button class="Game-keyboard-button" type="button" id="a">P</button>
+            </div>
+            <div class="Game-keyboard-row">
+              <button class="Game-keyboard-button" type="button" id="a">A</button>
+              <button class="Game-keyboard-button" type="button" id="a">S</button>
+              <button class="Game-keyboard-button" type="button" id="a">D</button>
+              <button class="Game-keyboard-button" type="button" id="a">F</button>
+              <button class="Game-keyboard-button" type="button" id="a">G</button>
+              <button class="Game-keyboard-button" type="button" id="a">H</button>
+              <button class="Game-keyboard-button" type="button" id="a">J</button>
+              <button class="Game-keyboard-button" type="button" id="a">K</button>
+              <button class="Game-keyboard-button" type="button" id="a">L</button>
+              <button class="Game-keyboard-button" type="button" id="a">Ñ</button>
+            </div>
+            <div class="Game-keyboard-row">
+              <button class="Game-keyboard-button" type="button" id="a">Z</button>
+              <button class="Game-keyboard-button" type="button" id="a">X</button>
+              <button class="Game-keyboard-button" type="button" id="a">C</button>
+              <button class="Game-keyboard-button" type="button" id="a">V</button>
+              <button class="Game-keyboard-button" type="button" id="a">B</button>
+              <button class="Game-keyboard-button" type="button" id="a">N</button>
+              <button class="Game-keyboard-button" type="button" id="a">M</button>
             </div>
           </div>
         </div>
