588e5281edb24bdd94426b73dadb03d895200a07
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-04-29 11:03:04 +0200

    teclado general app.js

--- Práctica/langchain-app/src/App.js
+++ Práctica/langchain-app/src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="Game">
+        <div class="Game-option Game-options-playing">
+
+        </div>
+        <div class="game-container-outer">
+          <div class="game-container-inner">
+
+          </div>
+          <div class="Game-challenge-bar">
+
+          </div>
+          <div class="Game-keyboard">
+            <div class="Game-keyboard-row">
+              <button class="Game-keyboard-button" type="button" id="a">Q</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    
   );
 }
 
