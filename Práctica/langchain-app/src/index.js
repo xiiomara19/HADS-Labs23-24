9295fa149b4b8f4bc4534f0bc9c30b701c86e60b
heyyder <efernandez200@ikasle.ehu.eus>
2024-05-02 20:14:09 +0200

    organizando los ficheros

--- Práctica/langchain-app/src/index.js
+++ Práctica/langchain-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Settings from './settings';
@@ -8,6 +8,7 @@ import Info from './info';
 import Statistics from './statistics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 function renderSettings() {
   root.render(
     <React.StrictMode>
--- Práctica/langchain-app/src/settings.js
+++ Práctica/langchain-app/src/settings.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './icons/logo.svg';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a>
+        <p>
           SETTINGS
-        </a>
+        </p>
       </header>
     </div>
   );
