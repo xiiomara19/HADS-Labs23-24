3f8d6f8bbd1d4efb3986142e07002bc2355e8453
heyyder <efernandez200@ikasle.ehu.eus>
2024-05-06 11:11:22 +0200

    modularizacion

---
+++ Práctica/langchain-app/src/elements/Keyboard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Keyboard() {
+  return (
+          <div className="Game-keyboard">
+            <div className="Game-keyboard-row">
+              <button className="Game-keyboard-button" type="button" id="a">Q</button>
+              <button className="Game-keyboard-button" type="button" id="a">W</button>
+              <button className="Game-keyboard-button" type="button" id="a">E</button>
+              <button className="Game-keyboard-button" type="button" id="a">R</button>
+              <button className="Game-keyboard-button" type="button" id="a">T</button>
+              <button className="Game-keyboard-button" type="button" id="a">Y</button>
+              <button className="Game-keyboard-button" type="button" id="a">U</button>
+              <button className="Game-keyboard-button" type="button" id="a">I</button>
+              <button className="Game-keyboard-button" type="button" id="a">O</button>
+              <button className="Game-keyboard-button" type="button" id="a">P</button>
+            </div>
+            <div className="Game-keyboard-row">
+              <button className="Game-keyboard-button" type="button" id="a">A</button>
+              <button className="Game-keyboard-button" type="button" id="a">S</button>
+              <button className="Game-keyboard-button" type="button" id="a">D</button>
+              <button className="Game-keyboard-button" type="button" id="a">F</button>
+              <button className="Game-keyboard-button" type="button" id="a">G</button>
+              <button className="Game-keyboard-button" type="button" id="a">H</button>
+              <button className="Game-keyboard-button" type="button" id="a">J</button>
+              <button className="Game-keyboard-button" type="button" id="a">K</button>
+              <button className="Game-keyboard-button" type="button" id="a">L</button>
+              <button className="Game-keyboard-button" type="button" id="a">Ñ</button>
+            </div>
+            <div className="Game-keyboard-row">
+              <button className="Game-keyboard-button" type="button" id="a">⌫</button>
+              <button className="Game-keyboard-button" type="button" id="a">Z</button>
+              <button className="Game-keyboard-button" type="button" id="a">X</button>
+              <button className="Game-keyboard-button" type="button" id="a">C</button>
+              <button className="Game-keyboard-button" type="button" id="a">V</button>
+              <button className="Game-keyboard-button" type="button" id="a">B</button>
+              <button className="Game-keyboard-button" type="button" id="a">N</button>
+              <button className="Game-keyboard-button" type="button" id="a">M</button>
+              <button className="Game-keyboard-button" type="button" id="a">Enter</button>
+            </div>
+          </div>
+  )
+}
+
+export default Keyboard
