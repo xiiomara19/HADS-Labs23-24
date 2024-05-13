6237e0053a6c3407d60eef0d4ee90858a008693e
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-05-13 19:13:43 +0200

    se han eliminado las funciones innecesarias

--- Práctica/langchain-app/src/Quordle.js
+++ Práctica/langchain-app/src/Quordle.js
@@ -1,50 +1,3 @@
-function sameLetterAndPosition(word, letter, pos) {
-    if (word[pos] === letter) {
-        return true;
-    }
-    else return false;
-}
-
-function sameLetterWrongPosition(word, letter, pos){
-    let array = [];
-    let i=0;
-
-    while (i < 5) {
-        if (word[i] === letter && i !== pos) {
-            array.push(i);
-            console.log(letter);
-        }
-        i++;
-    }
-    console.log(array);
-    return array;
-}
-
-function Quordle(word, letter, pos, id) {
-    let cell = document.getElementById(id);
-    let row = cell.parentElement;
-
-    if (sameLetterAndPosition(word, letter, pos)) {
-        cell.readOnly = true;
-        cell.classList.add("letter-correct");
-        console.log("correct");
-        //return "green";
-    }
-    else if (sameLetterWrongPosition(word, letter, pos).length > 0) {
-        cell.readOnly = true;
-        cell.classList.add("letter-elsewhere");
-        console.log("elsewhere");
-        //return "yellow";
-    }
-    else {
-        cell.readOnly = true;
-        cell.classList.add("letter-absent");
-        console.log("absent");
-        //return "grey";
-    }
-    row.classList.remove("Row-current");
-
-}
 
 export const boardBegininig = [
     ["", "", "", "", "",        "", "", "", "", ""],
@@ -69,5 +22,3 @@ export const boardBegininig = [
     ["", "", "", "", "",        "", "", "", "", ""]
   
 ]
-
-export default Quordle;
