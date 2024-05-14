507e1112a6d06c38670e28c89c2d5694a348e32a
heyyder <efernandez200@ikasle.ehu.eus>
2024-05-14 13:23:58 +0200

    propmpt creado para primer try

--- Práctica/langchain-app/src/GroqFuncions.js
+++ Práctica/langchain-app/src/GroqFuncions.js
@@ -27,7 +27,7 @@ const model = new ChatGroq({
     apiKey: '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',
   });
   
-const prompt = ChatPromptTemplate.fromMessages([
+  const prompt = ChatPromptTemplate.fromMessages([
     ["user", "You are playing the game Quordle. "+ 
     "You have to guess the 4 words that are hidden in the board. "+ 
     "You can guess a word by typing the word in the chat. The game "+
@@ -37,11 +37,11 @@ const prompt = ChatPromptTemplate.fromMessages([
     "that word wil be used as guess for the 4 hidden words. "+
     "After each attempt you will be provided the results of that attempt: "+
     "for each word you will receive feedbaack on: "+
-    " 1. how many letters are in the word but in the wrong postion, keyword used will be yellow "+
-    " 1. how many letters are in the word in the correct postion, keyword used will be green "+
-    " 1. how many letters are not in the word, keyword used will be grey "+
+    " 1. which letters are in the word but in the wrong postion, keyword used will be yellow "+
+    " 2. which letters are in the word in the correct postion, keyword used will be green "+
+    " 3. which letters are not in the word, keyword used will be grey "+
     "The words are in Spanish and 5 letters long. The words are not repeated. "+
-    " The word you provide mmust have exactly 5 letters long andd exist in the Spanish language. "],
+    " The word you provide must have exactly 5 letters long andd exist in the Spanish language. "],
 ]);
 
 export const fetchData = async () => {
@@ -49,5 +49,5 @@ export const fetchData = async () => {
     const response = await chain.invoke({
         input: "Make your first guess.",
     });
-    console.log("response", response);
+    console.log("response", response.content);
 };
