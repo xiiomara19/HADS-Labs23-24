4ed54b7cc796feca81e0f39428b7a8bafbfb1615
Daria <dpaslavska001@ikasle.ehu.eus>
2024-05-15 16:13:06 +0200

    devuelve palabra desde json de la ia

--- Práctica/backend/index.js
+++ Práctica/backend/index.js
@@ -5,13 +5,67 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 const ChatGroq = require('groq-sdk');
+const path = require("path");
+const fs = require("fs");
+const ChatPromptTemplate = require("@langchain/core/prompts");
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  //getDiccionary(res);
+  //getAnswerFromGroq(res);
+  //getGroqChatCompletion(res);
+  
 });
 
 app.listen(port, () => {
   console.log('App listening at http://localhost:5000');
+  main();
 });
+
+const groq = new ChatGroq({
+  apiKey: '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',
+});
+
+
+
+async function getDiccionary(res) {
+
+  const data = fs.readFileSync("db.json", "utf8");
+  const obj = JSON.parse(data);
+  const str = JSON.stringify(obj.solutions);
+  res.send(str);
+
+
+}
+
+async function main() {
+  const chatCompletion = await getGroqChatCompletion();
+  // Print the completion returned by the LLM.
+  //const wordMatch = chatCompletion.content.match(/guess: (\w+)/);
+  //  console.log(wordMatch);
+  //  const word = wordMatch ? wordMatch[1] : '';
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+ // console.log("Guessed word", word);
+}
+async function getGroqChatCompletion() {
+
+  const data = fs.readFileSync("db.json", "utf8");
+  const obj = JSON.parse(data);
+  const str = JSON.stringify(obj.solutions);
+
+  return groq.chat.completions.create({
+      messages: [
+          {
+              role: "user",
+              content: "Take 1 word from this file" + str+ "Only write this word no other text",
+          }
+      ],
+      model: "llama3-8b-8192"
+  });
+}
+module.exports = {
+  main,
+  getGroqChatCompletion
+};
