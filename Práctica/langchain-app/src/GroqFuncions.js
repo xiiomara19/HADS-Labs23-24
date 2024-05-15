ca50d96b0d8fe58e2e036f06b552a045224348d9
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-05-15 14:09:59 +0200

    mas cambios

--- Práctica/langchain-app/src/GroqFuncions.js
+++ Práctica/langchain-app/src/GroqFuncions.js
@@ -1,7 +1,7 @@
 
 import {ChatGroq} from '@langchain/groq';
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { AppContext } from '../App';
+import { AppContext } from './App';
 import { useContext } from 'react';
 
 const {wordSet} = useContext(AppContext);
